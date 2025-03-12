ce8820de57cc4febe59481a1e06e34cd2a9bb2cd
Sanjusree09 <sanjusree2626@gmail.com>
2025-03-12 11:53:44 +0530

    Update

--- src/app/sun-weather-tracker/specific-date-timezone/specific-date-timezone.component.ts
+++ src/app/sun-weather-tracker/specific-date-timezone/specific-date-timezone.component.ts
@@ -20,7 +20,7 @@ import moment from 'moment-timezone';
     <label class="inline-flex items-center cursor-pointer" (click)="toggleme()">
       <input type="checkbox" value="" class="sr-only peer">
       <div (click)="toggleme()" class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
-      <span class="ps-3 text-sm font-medium text-gray-900 dark:text-gray-300">Date Range</span>
+      <span class="ps-3 text-sm font-medium text-white dark:text-white">Date Range</span>
     </label>
   </div>
 
@@ -35,7 +35,7 @@ import moment from 'moment-timezone';
 
 
   <div *ngIf="error">
-    <span class="text-red-500">*{{ error }}</span>
+    <span class="text-red-500 flex justify-center">*{{ error }}</span>
   </div>
 
  
